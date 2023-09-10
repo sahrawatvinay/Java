2d92468d4b2473253860adf59db09dacab62c4d6
vinay <vinaysahrawat183a@gmail.com>
2023-09-10 19:53:35 -0400

    structure updated

---
+++ JavaPractise/src/basics/MainProgram.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class MainProgram {
+	/*
+	 * first basic program
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GG");
+	}
+
+}
