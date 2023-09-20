994ad9d687ab02024a0219e1e6be049a75759b59
vinay <vinaysahrawat183a@gmail.com>
2023-09-20 14:48:41 -0400

    array list basic implementation

---
+++ Arrays/src/collections/arrayListImplementation.java
@@ -0,0 +1,32 @@
+package collections;
+import java.util.*;
+public class arrayListImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		System.out.println(list);
+		for(Integer i:list) {
+			System.out.println(i);
+			Class<?> objClass = i.getClass();
+			System.out.println(objClass);
+		}
+		list.size();
+		list.get(2);
+		list.add(1,9);
+		System.out.println(list);
+		list.set(0, 69);
+		list.remove(list.size()-1);
+		System.out.println(list);
+		Iterator<Integer> it = list.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+
+}
---
+++ DOMJudge/src/pod2/checkForSquare.java
@@ -0,0 +1,16 @@
+package pod2;
+import java.util.*;
+public class checkForSquare {
+	static boolean checkIfPointsFormSquare(int[][] sqArray) {
+		boolean result = false;
+		
+		return result;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer>[][] arrPoints = new ArrayList[4][2];
+		
+	}
+
+}
