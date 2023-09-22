69731d8a839d7a01e3fda039aa3eb9dddd6e3691
vinay <vinaysahrawat183a@gmail.com>
2023-09-21 22:03:55 -0400

    pod2 q2

---
+++ DOMJudge/src/pod2/arrayPairSum.java
@@ -0,0 +1,22 @@
+package pod2;
+import java.util.*;
+public class arrayPairSum {
+    static int getPairSumResult(int[] arr){
+        int nSum = 0;
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length ; i += 2 ){
+            nSum += Math.min(arr[i], arr[i+1]);
+        }
+        return nSum;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int nSize = sc.nextInt();
+        int[] nArr = new int[nSize];
+        for(int i=0; i<nSize; i++){
+            nArr[i] = sc.nextInt();
+        }
+        int nResult = getPairSumResult(nArr);
+        System.out.println(nResult);
+    }
+}
